4c88bbb0ab41ff8e167e04236ffe7d84a8842a59
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-07-10 12:27:07 -0300

    Adds count_elem/2 to count list elements

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    count_elem(list, 0)
+  end
+
+  defp count_elem([], acc), do: acc
+
+  defp count_elem([_head|tail], acc) do
+    count_elem(tail, acc + 1)
   end
 end
