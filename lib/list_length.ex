fd783ead0b1bad87f065106bca3f170d071faf99
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-07-10 12:29:51 -0300

    Adds spec annotation for call/1 and count_elem/2

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,10 +1,13 @@
 defmodule ListLength do
+  @spec call(list()) :: integer()
   def call(list) do
     count_elem(list, 0)
   end
 
+  @spec count_elem(list(), integer()) :: integer()
   defp count_elem([], acc), do: acc
 
+  @spec count_elem(list(), integer()) :: integer()
   defp count_elem([_head|tail], acc) do
     count_elem(tail, acc + 1)
   end
